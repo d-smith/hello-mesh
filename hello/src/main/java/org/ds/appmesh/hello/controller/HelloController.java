0a0ec7db3c9116947d517c04bc7596c2d6e856de
d-smith <doug.smith.mail@gmail.com>
2021-12-16 13:38:40 -0800

    added hello service

---
+++ hello/src/main/java/org/ds/appmesh/hello/controller/HelloController.java
@@ -0,0 +1,45 @@
+package org.ds.appmesh.hello.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class HelloController {
+    private static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    private String NAME_ENDPOINT = System.getenv("NAME_ENDPOINT");
+    private String GREETING_ENDPOINT = System.getenv("GREETING_ENDPOINT");
+
+    private static RestTemplate rest  = new RestTemplate();
+    private static HttpEntity<String> requestEntity = new HttpEntity<String>("");
+
+
+    private static String getDataFromEndpoint(String endpoint, String path, String errDefault) {
+        logger.info("get data from {}, path {}", endpoint, path);
+        ResponseEntity<String> responseEntity = rest.exchange(endpoint + path, HttpMethod.GET, requestEntity, String.class);
+        if(responseEntity.getStatusCode() == HttpStatus.OK) {
+            return responseEntity.getBody();
+        } else {
+            logger.info("Non-ok staus returned - {}", responseEntity.getStatusCode());
+            logger.info(responseEntity.getBody());
+            return errDefault;
+        }
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        String name = getDataFromEndpoint(NAME_ENDPOINT, "/name", "some name");
+        logger.info("got name {}", name);
+        String greeting = getDataFromEndpoint(GREETING_ENDPOINT,"/greeting", "some greeting");
+        logger.info("got greeting {}", greeting);
+
+        return new ResponseEntity<>(greeting + ", " + name, HttpStatus.OK);
+    }
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,14 @@
 # hello-mesh
 
 Shameless rip-off of Nathan Peck's [Greeter App Mesh CDK](project), writting in Java.
+
+
+## Run containers locally
+
+```
+docker network create hello-net
+docker run --name name --network hello-net -p 8080:8080 name
+docker run --name greeting --network hello-net -p 8081:8080 greeting
+docker run --network hello-net  --env NAME_ENDPOINT=http://name:8080 --env GREETING_ENDPOINT=http://greeting:8080 -p 3000:8080 hello
+```
+
